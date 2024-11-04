853475ed0e7f18890f0b51b9aaf18bfddde66a71
liuqingpo <1432003391@qq.com>
2024-11-04 19:36:20 +0800

    feat：用户鉴权功能

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { requiredPermission, RequireLogin } from './custom.decorator';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('aaa')
+  // @SetMetadata('require-login', true)
+  // @SetMetadata('require-permission', ['ddd'])
+  @RequireLogin()
+  @requiredPermission('ddd')
+  aaaa() {
+    return 'aaa';
+  }
+
+  @Get('bbb')
+  bbb() {
+    return 'bbb';
   }
 }
